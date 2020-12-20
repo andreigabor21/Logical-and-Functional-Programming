2d8d9a2da20b421e05b2a9d68fdfc7a9b477a24e
Andrei <andreigabor36@gmail.com>
2020-12-21 01:12:47 +0200

    some changes

--- Lisp/cursuri/curs11/matrix_product.lisp
+++ Lisp/cursuri/curs11/matrix_product.lisp
@@ -10,22 +10,9 @@
 
 (print (coloane '((1 2) (3 4))))
 
-(defun prod(L1 L2)
-    (cond
-        ((null (car l1)) nil)
-        (T (cons (mapcar #'(lambda (L) 
-                        (apply #'+ (mapcar #'* (car L1) L))
-                    )
-                    L2
-                )
-                (prod (cdr l1) l2)
-            )
-        )
-    )
-) 
 
-(defun product(L1 L2)
-    (prod L1 (coloane L2))
-)
+;; (defun product(L1 L2)
+;;     (prod L1 (coloane L2))
+;; )
 
-(print (product '((1 2) (3 4)) '((2 -1) (3 1)) ))
+;; (print (product '((1 2) (3 4)) '((2 -1) (3 1)) ))
